84c13bc506f1955800f5825accf2637ba81c6673
JY1ee225 <nita911225@gmail.com>
2025-06-10 18:34:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,38 +27,3 @@ with st.expander("阿里山森林遊樂區"):
     st.image("ali-img-2-md.jpg")
 
 
-
-st.title("選擇日期區間")
-
-
-# 初始化 session_state
-#if 'start_date' not in st.session_state:
-#    st.session_state['start_date'] = date(2024, 1, 1)
-#if 'end_date' not in st.session_state:
-#    st.session_state['end_date'] = date.today()
-
-st.session_state['start_date'] = date(2024, 1, 1)
-st.session_state['end_date'] = date.today()
-
-
-# 日期選擇器
-start_date = st.date_input(label = "選擇起始日期", value = st.session_state['start_date'], min_value = date(2018, 1, 1), max_value = date.today())
-end_date = st.date_input(label = "選擇結束日期", value = st.session_state['end_date'], min_value = start_date, max_value = date.today())
-
-# 儲存使用者選擇
-st.session_state['start_date'] = start_date
-st.session_state['end_date'] = end_date
-
-st.success(f"目前選擇的日期區間為：{start_date} 到 {end_date}")
-
-
-st.title("利用擴充器示範")
-
-#st.image("網址")或是上傳資料然後打名稱
-with st.expander("展示gif檔"):
-    st.image("關西機場.gif")
-
-with st.expander("播放mp4檔"):
-    video_file = open("pucallpa.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
-    video_bytes = video_file.read()
-    st.video(video_bytes)
