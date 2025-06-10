89e8a80cb9b40a6c2158c7bfe2270b256f8d349d
JY1ee225 <nita911225@gmail.com>
2025-06-10 18:28:34 +0800

    Update 3_觀光指引.py

--- pages/3_觀光指引.py
+++ pages/3_觀光指引.py
@@ -2,9 +2,25 @@ import streamlit as st
 
 st.title("旅遊指引")
 
+st.header("阿里山森林遊樂區景點住宿等統整")
 st.markdown(
     """
     嘉義阿里山攻略》阿里山森林遊樂區景點住宿＆小火車時刻表
+    
+    https://www.kkday.com/zh-tw/blog/38246/asia-taiwan-chiayi-alishan-travel?srsltid=AfmBOoqaAVAH0xXemIirJULMjMZq7T3h1q3CuBrWal5MfXp2feEEfcIm
+    """
+)
+
+with st.expander("阿里山門票票價"):
+    st.image("螢幕擷取畫面 2025-06-10 182711.png")
+
+
+
+st.header("阿里山森林遊樂區景點住宿等統整")
+st.markdown(
+    """
+    嘉義阿里山攻略》阿里山森林遊樂區景點住宿＆小火車時刻表
+    
     https://www.kkday.com/zh-tw/blog/38246/asia-taiwan-chiayi-alishan-travel?srsltid=AfmBOoqaAVAH0xXemIirJULMjMZq7T3h1q3CuBrWal5MfXp2feEEfcIm
     """
 )
