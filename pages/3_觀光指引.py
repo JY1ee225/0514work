f1af94b0d2d02b1eea3a031bb67e5a5c3850e2ce
JY1ee225 <nita911225@gmail.com>
2025-06-10 18:33:03 +0800

    Update 3_觀光指引.py

--- pages/3_觀光指引.py
+++ pages/3_觀光指引.py
@@ -13,14 +13,22 @@ st.markdown(
 
 with st.expander("阿里山門票票價"):
     st.image("螢幕擷取畫面 2025-06-10 182711.png")
+    
+with st.expander("阿里山景點地圖"):
+    st.image("阿里山-景點-地圖.jpg")
 
 
 
-st.header("阿里山森林遊樂區景點住宿等統整")
+st.header("相關遊記推薦")
 st.markdown(
     """
-    嘉義阿里山攻略》阿里山森林遊樂區景點住宿＆小火車時刻表
-    
-    https://www.kkday.com/zh-tw/blog/38246/asia-taiwan-chiayi-alishan-travel?srsltid=AfmBOoqaAVAH0xXemIirJULMjMZq7T3h1q3CuBrWal5MfXp2feEEfcIm
-    """
+   【2025嘉義阿里山】阿里山景點地圖、小火車交通、雲海、日出
+   https://vivianjourney.tw/alishan-travel/
+
+   【阿里山景點推薦】上山避暑！7大景點、必吃美食、附近住宿一次看
+   https://www.funtime.com.tw/blog/funtime/alishan-route
+
+   阿里山小火車+日出美景三日遊
+   https://www.94iprofun.com/product_travel_detail.php?item=17
+   """
 )
