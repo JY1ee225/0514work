a58dabf1fd8b600c27ad3e10ce2a46a60d17763d
JY1ee225 <nita911225@gmail.com>
2025-06-10 11:21:13 +0800

    Update 4_眠月線.py

--- pages/4_眠月線.py
+++ pages/4_眠月線.py
@@ -3,3 +3,7 @@ import streamlit as st
 with st.expander("眠月線timelapse"):
     st.image("阿里山 (1).gif")
 
+with st.expander("眠月線google earth pro影像"):
+    video_file = open("pucallpa.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
+    video_bytes = video_file.read()
+    st.video(video_bytes)
