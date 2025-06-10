e58fe96e878fe5ad4466b80f3fce5caba8b0458f
JY1ee225 <nita911225@gmail.com>
2025-06-10 17:12:11 +0800

    Update 4_眠月線.py

--- pages/4_眠月線.py
+++ pages/4_眠月線.py
@@ -4,6 +4,6 @@ with st.expander("眠月線timelapse"):
     st.image("阿里山 (1).gif")
 
 with st.expander("眠月線google earth pro影像"):
-    video_file = open("pucallpa.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
+    video_file = open("Google Earth Pro 2025-06-10 11-19-52.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
     video_bytes = video_file.read()
     st.video(video_bytes)
