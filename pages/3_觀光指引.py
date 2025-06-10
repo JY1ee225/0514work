6d0979c80bf4fc5732d51caa1d2ba12e8fe0ea89
JY1ee225 <nita911225@gmail.com>
2025-06-10 18:25:04 +0800

    Update 3_觀光指引.py

--- pages/3_觀光指引.py
+++ pages/3_觀光指引.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("旅遊指引")
+
+st.markdown(
+    """
+    嘉義阿里山攻略》阿里山森林遊樂區景點住宿＆小火車時刻表
+    https://www.kkday.com/zh-tw/blog/38246/asia-taiwan-chiayi-alishan-travel?srsltid=AfmBOoqaAVAH0xXemIirJULMjMZq7T3h1q3CuBrWal5MfXp2feEEfcIm
+    """
+)
